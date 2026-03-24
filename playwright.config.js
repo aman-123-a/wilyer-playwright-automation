f46f2c063a4a203fda97aa49152c06058ff73348
Aman Kumar <amankumarbsrbsr@gmail.com>
2026-03-24 12:03:57 +0530

    Roless

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from '@playwright/test';
+
 export default defineConfig({
   testDir: './tests',
+
   timeout: 60000,
+  expect: {
+    timeout: 10000
+  },
+
   retries: 1,
+
   use: {
     headless: true,
     screenshot: 'only-on-failure',
-    video: 'retain-on-failure'
-  }
+    video: 'retain-on-failure',
+    trace: 'on-first-retry'
+  },
+
+  reporter: 'html'
 });
